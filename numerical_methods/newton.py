3c6b50eb0b630d9fa925d6d3460597b8e6c83c26
Victor Cunha <victor_matheus@live.com>
2026-02-16 20:42:24 -0300

    Newton/Raphson

--- numerical_methods/newton.py
+++ numerical_methods/newton.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+tol = 0.001
 xi = []
+xmin, xmax, ymin, ymax = -2.5, 2.5, -2, 2
+domain = np.linspace(xmin, xmax, 100)
+
 
 def f(x):
     return 2*np.sin(x) - x
@@ -11,14 +14,37 @@ def f(x):
 def dfdx(x):
     return 2*np.cos(x) - 1
 
-x0 = 2.2
 
-for i in range(100):
+tangente = lambda x: dfdx(x) * x + f(0) 
+    
+
+x0 = np.float64(1.8)
+xi.append(x0)
+eps = x0
+iteration = 0
+
+while tol <= eps: 
     x = x0 - f(x0) / dfdx(x0)
-    x0 = x
+    eps = abs(x0 - x)
+    print(eps)
     xi.append(x)
+    x0 = x
+    iteration += 1
+    
+y = f(domain)
+yi = f(xi)
+print(iteration)
+print(xi)
 
-y = f(xi)
-plt.scatter(xi, y)
+
+################## PLOTAGEM ###############################
+fig, ax = plt.subplots()
+plt.axis([xmin, xmax, ymin, ymax])
+plt.axhline(0, color='black',linewidth=1) # Eixo X horizontal
+plt.axvline(0, color='black',linewidth=1) # Eixo Y vertical
+plt.plot(domain, y)
+plt.plot(xi, tangente(xi))
+plt.scatter(xi, yi)
 plt.show()
-print(x)
+############################################################
+
